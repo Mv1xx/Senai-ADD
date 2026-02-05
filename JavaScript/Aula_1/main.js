3bc95a8c17dbf487c54307a41151d02c7f413eaf
Maria <mariaclaravskis.@gmail.com>
2026-02-05 11:39:14 -0300

    Aula JS

---
+++ JavaScript/Aula_1/main.js
@@ -0,0 +1,29 @@
+// possivel alterar valor
+var cidade = "Sao Paulo";
+
+// nao é possivel alterar o valor
+let idade = 30;
+const PI = 3.14;
+
+//array
+let frutas = ['maça', 'banana', 'uva'];
+
+//objeto
+let pessoas = {
+    nome: 'Joao',
+    idade: 28,
+    profissao: 'Engenheiro'
+};
+
+//funcao 
+function soma(a, b){
+    return a + b;
+};
+
+//função anonima atribuida
+const mult = function (a, b){
+    return a * b;
+};
+
+//arrow function
+const sub = (a, b) => a - b;
