4c69cd99fa4fd32f0f39bcd4d4cd0ce297f78978
Maria <mariaclaravskis.@gmail.com>
2026-03-25 10:26:38 -0300

    prova

---
+++ Prova/Exercicio 1/main.js
@@ -0,0 +1,31 @@
+let caro = document.getElementById('caro').value
+let qntd = document.getElementById('qntd').value
+let estoqueTotal = document.getElementById('estoqueTotal').value
+let button = document.getElementById('button')
+
+let maisCaro = []
+let maiorQntd = []
+let maisEstoque = []
+
+const dados = require('./dados.json')
+
+// const maisCaroFilter = dados.map((dados) =>{
+//     return 
+// })
+
+// console.log(maisCaroFilter)
+
+button.addEventListener('click', () => {
+        
+})
+
+
+
+
+
+
+
+
+
+
+
