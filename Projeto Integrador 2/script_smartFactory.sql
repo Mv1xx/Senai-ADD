b19dd1dae446850460c0205286d20e359b4e5ca9
Maria <mariaclaravskis.@gmail.com>
2026-06-03 10:55:52 -0300

    criando database

---
+++ Projeto Integrador 2/script_smartFactory.sql
@@ -0,0 +1,13 @@
+use smartfactory;
+create table equipamento
+(id_equipamento int primary key auto_increment);
+create table alerta
+(id_alerta int primary key auto_increment);
+create table sensor
+(id_sensor int primary key auto_increment);
+create table tipo_sensor
+(id_tipo_sensor int primary key auto_increment);
+create table leitura
+(id_leitura int primary key auto_increment);
+create table setor
+(id_setor int primary key auto_increment);
